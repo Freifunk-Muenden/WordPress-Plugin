47e70e0ed565b0685e0c0feeec660e82f519bb95
Kristian Stöckel <kristian@stoeckel-mail.de>
2017-04-11 11:47:24 +0200

    +Nodeliste

--- includes/Shortcodes/sc.stats.php
+++ includes/Shortcodes/sc.stats.php
@@ -8,10 +8,42 @@ class FF_SC_Stats {
     public static function render() {
         echo '<p>Wir möchten an dieser Stelle von Zeit zu Zeit immer mehr Statistiken integrieren.</p>';
         FF_SC_Stats::printGlobalGraph();
+        echo '<hr/>';
+        FF_SC_Stats::printNodeList();
     }
 
     private static function printGlobalGraph() {
         echo '<img src="/ffapi/data/nodes/globalGraph.png" alt="Anzahl der Nodes und Clients der letzten 7 Tage" />';
+    }
+
+    private static function printNodeList() {
+        $nodes = FF_Nodes::getNodes();
+        echo '<table class="table" id="nodelist">';
+        echo '<thead>';
+        echo '<tr>';
+        echo '<th>Hostname</th>';
+        echo '<th>Clients</th>';
+        echo '<th></th>';
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+        foreach ($nodes as $node) {
+            echo '<tr>';
+            echo '<td><b>' . $node->getHostname() . '</b></td>';
+            echo '<td data-order="' . $node->getClientCount() . '">' . $node->getClientCount() . ' Clients</td>';
+            echo '<td data-order="' . $node->isOnline() . '">' . ($node->isOnline() ? 'Online!' : 'Offline!') . '</td>';
+            echo '</tr>';
+        }
+        echo '</tbody>';
+        echo '</table>';
+        echo '<script type="text/javascript">
+            $("#nodelist").DataTable({
+                "language": {
+                    "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/German.json"
+                },
+                "order": [[ 1, "desc" ]]
+            });
+        </script>';
     }
 
 }
